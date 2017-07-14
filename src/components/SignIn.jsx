60857e5298e735ac501c878278d81442d62cb816
itsca <itscae@gmail.com>
2017-07-14 12:04:22 -0600

    Added basic components

---
+++ src/components/SignIn.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default class SignIn extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div>SignIn</div>
+    );
+  }
+}
--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import ReactDom from 'react-dom';
+//For V4 improvement
+// import { BrowserRouter } from 'react-router-dom';
+import App from './components/App.jsx';
+import SignIn from './components/SignIn.jsx';
+import SignUp from './components/SignUp.jsx';
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+ReactDom.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>, document.getElementById('root')
+)
