b7fb89f815b1659ca8191d8e4cfd7a257fac337a
itsca <itscae@gmail.com>
2017-07-14 12:21:45 -0600

    Added simple Router

--- src/index.js
+++ src/index.js
@@ -2,12 +2,15 @@ import React from 'react';
 import ReactDom from 'react-dom';
 //For V4 improvement
 // import { BrowserRouter } from 'react-router-dom';
+import { Router, Route, browserHistory } from 'react-router';
 import App from './components/App.jsx';
 import SignIn from './components/SignIn.jsx';
 import SignUp from './components/SignUp.jsx';
 
 ReactDom.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>, document.getElementById('root')
+  <Router path="/" history={browserHistory}>
+  <Route path="/app" component={App} />
+  <Route path="/signin" component={SignIn} />
+  <Route path="/signup" component={SignUp} />
+  </Router>, document.getElementById('root')
 )
