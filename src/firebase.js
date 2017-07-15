7704694ac766758b87a9da25f4c5419791713268
itsca <itscae@gmail.com>
2017-07-15 17:03:29 -0600

    Added firebase Api and on auth logic.

---
+++ src/firebase.js
@@ -0,0 +1,12 @@
+import * as firebase from 'firebase';
+
+const config = {
+    apiKey: "AIzaSyBRyFb7pWHmyxttmknddBAIEW5YyfcAQmY",
+    authDomain: "goalreactapp.firebaseapp.com",
+    databaseURL: "https://goalreactapp.firebaseio.com",
+    projectId: "goalreactapp",
+    storageBucket: "goalreactapp.appspot.com",
+    messagingSenderId: "863895723773"
+  };
+
+export const firebaseApp = firebase.initializaApp(config);
--- src/index.js
+++ src/index.js
@@ -3,9 +3,21 @@ import ReactDom from 'react-dom';
 //For V4 improvement
 // import { BrowserRouter } from 'react-router-dom';
 import { Router, Route, browserHistory } from 'react-router';
+import { firebaseApp } from './firebase';
 import App from './components/App.jsx';
 import SignIn from './components/SignIn.jsx';
 import SignUp from './components/SignUp.jsx';
+
+firebase.auth().onAuthStateChange(user => {
+  if (user) {
+    console.log('user has signed in or up', user);
+  } else {
+    console.log('user has signed out or still need to sign in.');
+  }
+})
+
+
+
 
 ReactDom.render(
   <Router path="/" history={browserHistory}>
