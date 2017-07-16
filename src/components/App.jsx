bc9b8c6e70e4003f7161af78607c524a7f4cf0f5
itsca <itscae@gmail.com>
2017-07-16 16:43:16 -0600

    Added sign out functionality and redirection of URLs based on the auth status.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,13 +1,23 @@
 import React from 'react';
+import { firebaseApp } from '../firebase.js';
 
 export default class App extends React.Component {
   constructor(props) {
     super(props);
   }
 
+ signOut() {
+   firebaseApp.auth().signOut();
+ }
+
   render() {
     return (
-      <div>App</div>
+      <div>
+        App
+        <br/>
+        <button className="btn btn-danger"
+          onClick={() => this.signOut()}>Sign Out</button>
+      </div>
     );
   }
 }
