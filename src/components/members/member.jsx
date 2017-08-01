c0d37ec364f12ab5ffdb8253a8b5026929606ff2
itsca <itscae@gmail.com>
2017-07-31 20:45:51 -0600

    Added member component with online and admin glyphs to member list

---
+++ src/components/members/member.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import {Glyphicon} from 'react-bootstrap';
+
+class TeamMember extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div>
+        <Glyphicon glyph="user" style={this.props.member.online ? {color: '#4caf50'} : {color: '#9e9e9e'}}/>
+        {' ' + this.props.member.userName + ' '}
+        {this.props.member.admin ? <Glyphicon glyph="star" style={{color: '#eefd08'}}/> : ''}
+      </div>);
+  }
+}
+
+export default TeamMember;
--- src/components/members/memberList.jsx
+++ src/components/members/memberList.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { firebaseApp, usersRef, teamsRef } from '../../firebase.js';
+import TeamMember from './member.jsx';
+import AddMember from './addMember.jsx';
 
 class MemberList extends React.Component {
   constructor(props) {
@@ -14,42 +16,64 @@ class MemberList extends React.Component {
     let members = [];
     teamsRef.child(this.props.tid).child('members').on('value', snap => {
       snap.forEach( member => {
-        console.log('MLC VAL', member.val().uid);
         if (member.val().uid != undefined) {
           usersRef.child(member.val().uid).on('value', snap2 => {
             const {userName, online} = snap2.val();
-            const uid = snap2.key;
-            members.push({uid, userName, online});
+            const memberUid = snap2.key;
+            let admin = false;
+            if (this.props.user != undefined) {
+              if ( memberUid === this.props.user.uid) {
+                admin = true;
+              }
+            }
+            members.push({memberUid, userName, online, admin});
             this.setState({teamMembers : members});
           })
         }
       })
     })
+    teamsRef.child(this.props.tid).child('admin').on('value', snap => {
+      if (this.props.user != undefined) {
+        if (this.props.user.uid === snap.val()) {
+          this.setState({admin : true});
+        }
+      }
+    })
+  }
+
+  checkForAdd() {
+    if (this.state.admin === true) {
+      return (
+        <AddMember/>
+      );
+    }
   }
 
   render() {
     return (
       <div>
+        {this.checkForAdd()}
         <h5>Members</h5>
-        {console.log(this.state.teamMembers)}
-        {
-          this.state.teamMembers.map((member, index) => {
-              const memberName = member.userName;
-              return (
-                <div className="list-item" key={index}>
-                  <h4>{memberName}</h4>
-                </div>
-              )
-            }
-          )
-        }
+        <ul className="list-group">
+          {
+            this.state.teamMembers.map((member, index) => {
+              const {memberUid, userName, online, admin} = member;
+                return (
+                  <li className="list-group-item" key={index}>
+                    <TeamMember member={member}/>
+                  </li>
+                )
+              }
+            )
+          }
+        </ul>
       </div>
     );
   }
 }
 
 function mapStateToProps(state) {
-  console.log('MLC', state);
+  //console.log('MLC', state);
   const  { user } = state;
   return {
     user
